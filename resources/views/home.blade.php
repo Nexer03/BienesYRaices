82ba9bb14dcb81a45fb0ad140767820548215a87
Jesus Peña <imnex03@gmail.com>
2025-10-10 18:20:09 -0600

    Ajustes de rama pesonal, con el env. y instale el tailwind.. clone repositiorios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
   </head>
-  <body>
-    <h1 class="text-3xl font-bold underline bg-fuchsia-200">
-      Hello world! crtuel
+  <body class="bg-black text-white p-8">
+
+    <h1 class="text-3xl font-bold underline">
+      Esta es la pagina principal de Bienes Raices, se encuentra en resources/views/home.blade.php.
+      <P>
+        DEBES VER ESTO EN FONDO NEGRO Y LETRAS NEGRAS PARA SABER Q TIENE EL TAILWIND BIEN CONFIGURADO
+      </P>
+      
+      
     </h1>
   </body>
 </html>
