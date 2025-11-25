a07f62a6f9f42e5765143903b2456ab4602403fc
Jesus Peña <imnex03@gmail.com>
2025-11-25 02:45:26 -0600

    fix: Quitar el inkedin

--- resources/views/components/main-footer.blade.php
+++ resources/views/components/main-footer.blade.php
@@ -24,8 +24,6 @@
                     class="hover:text-blue-500 dark:hover:text-blue-400 transition-colors">
                         <i class="fab fa-instagram"></i>
                     </a>
-
-                <a href="#" class="hover:text-blue-500 dark:hover:text-blue-400 transition-colors"><i class="fab fa-linkedin-in"></i></a>
                 <a href="#" class="hover:text-blue-500 dark:hover:text-blue-400 transition-colors"><i class="fab fa-x-twitter"></i></a>
             </div>
         </div>
